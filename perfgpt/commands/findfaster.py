bf7eb1c7920fef90d77005719b029046a936206d
Sean Heelan <sean.heelan@elastic.co>
2023-03-27 17:18:08 +0100

    Update findfaster prompts
    There are two changes:
    
    1. Add the extra information that the suggested library should
    capable of replacing the target library.
    2. Ask for a summary of the target library.
    
    These changes combined seem to result in better output, and avoid
    significantly unrelated libraries/programs being suggested.
    
    The second modification also results in more useful output, in case
    whomever is using the tool isn't clear on what the library they want
    to replace does.

--- perfgpt/commands/findfaster.py
+++ perfgpt/commands/findfaster.py
@@ -15,18 +15,22 @@ def add_to_command_parser(subparsers):
 
 software_type_prompts = {
     "program": """What are the fastest and most memory-efficient programs that provide
-                the same functionality as {target}, specifically those that use SIMD instructions
-                or are optimized for high performance? Output the suggested programs in a list.
-                For each program, give a summary.""",
+                the same functionality as {target} and can be used to replace it? Specifically
+                I am interested in those those that use SIMD instructions or are optimized for
+                scalability and high performance? Provide a summary of {target}, then output
+                the suggested programs in a list. For each program, give a summary.""",
     "library": """What are the fastest and most memory-efficient libraries that provide
-                the same functionality as {target}, specifically those that use SIMD instructions
-                or are optimized for high performance? Output the suggested libraries in a list.
-                For each library, give a summary.""",
+                the same functionality as {target}, and can be used to replace it. Specifically
+                I am interested in those that use SIMD instructions or are optimized for high
+                performance and scalability? Provide a summary of {target}, then output
+                the suggested libraries in a list. For each library, give a summary.""",
     "pylibrary": """What are the fastest and most memory-efficient Python libraries that
-                provide the same functionality as Python's {target} library, specifically those that use
-                SIMD instructions or are optimized for high performance? I'm also interested in any
-                lightweight, low-level libraries that may provide better performance than pure-Python
-                libraries. Output the suggested libraries in a list. For each library, give a summary."""
+                provide the same functionality as Python's {target} library and can be used to replace
+                it. Specifically I am interested in those that use SIMD instructions or are optimized
+                for high performance and scalability? I'm also interested in any lightweight, low-level
+                libraries that may provide better performance than pure-Python libraries. Provide a
+                summary of {target}, then output the suggested libraries in a list. For each library,
+                give a summary."""
 }
 
 
