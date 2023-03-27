bfad07fa88e9e89093322f4574c04a44d32bfd0c
Sean Heelan <sean.heelan@elastic.co>
2023-03-27 17:31:12 +0100

    Minor grammaticaly change in prompt

--- perfgpt/commands/findfaster.py
+++ perfgpt/commands/findfaster.py
@@ -15,7 +15,7 @@ def add_to_command_parser(subparsers):
 
 software_type_prompts = {
     "program": """What are the fastest and most memory-efficient programs that provide
-                the same functionality as {target} and can be used to replace it? Specifically
+                the same functionality as {target}, and can be used to replace it? Specifically
                 I am interested in those those that use SIMD instructions or are optimized for
                 scalability and high performance? Provide a summary of {target}, then output
                 the suggested programs in a list. For each program, give a summary.""",
@@ -25,7 +25,7 @@ software_type_prompts = {
                 performance and scalability? Provide a summary of {target}, then output
                 the suggested libraries in a list. For each library, give a summary.""",
     "pylibrary": """What are the fastest and most memory-efficient Python libraries that
-                provide the same functionality as Python's {target} library and can be used to replace
+                provide the same functionality as Python's {target} library, and can be used to replace
                 it. Specifically I am interested in those that use SIMD instructions or are optimized
                 for high performance and scalability? I'm also interested in any lightweight, low-level
                 libraries that may provide better performance than pure-Python libraries. Provide a
