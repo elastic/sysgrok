cd9fc72f8476a4d1ddef05ddcd785392c3560673
Sean Heelan <sean.heelan@elastic.co>
2023-05-03 20:24:07 +0100

    Limit findfaster output to three suggestions
    The goal here is to reduce the likelihood of spurious output, but
    this also does reduce the number of chances the LLM has to get a
    useful answer. I may revisit this.

--- perfgpt/commands/findfaster.py
+++ perfgpt/commands/findfaster.py
@@ -18,19 +18,24 @@ software_type_prompts = {
                 the same functionality as {target}, and can be used to replace it? Specifically
                 I am interested in those those that use SIMD instructions or are optimized for
                 scalability and high performance? Provide a summary of {target}, then output
-                the suggested programs in a list. For each program, give a summary.""",
+                the suggested programs in a list. For each program, give a summary.
+                Suggest at most three programs. Only suggest programs that are confirmed
+                to be faster by several sources.""",
     "library": """What are the fastest and most memory-efficient libraries that provide
                 the same functionality as {target}, and can be used to replace it. Specifically
                 I am interested in those that use SIMD instructions or are optimized for high
                 performance and scalability? Provide a summary of {target}, then output
-                the suggested libraries in a list. For each library, give a summary.""",
+                the suggested libraries in a list. For each library, give a summary.
+                Suggest at most three libraries. Only suggest libraries that are confirmed
+                to be faster by several sources.""",
     "pylibrary": """What are the fastest and most memory-efficient Python libraries that
                 provide the same functionality as Python's {target} library, and can be used to replace
                 it. Specifically I am interested in those that use SIMD instructions or are optimized
                 for high performance and scalability? I'm also interested in any lightweight, low-level
                 libraries that may provide better performance than pure-Python libraries. Provide a
                 summary of {target}, then output the suggested libraries in a list. For each library,
-                give a summary."""
+                give a summary. Suggest at most three libraries. Only suggest libraries are confirmed
+                to be faster by several sources."""
 }
 
 
