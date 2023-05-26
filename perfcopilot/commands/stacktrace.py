aa0918e48ee8bba64f73d6c3563fbd6eee0964e6
Sean Heelan <sean@heelan.io>
2023-05-26 16:17:40 +0100

    Improve the stacktrace command prompt
    The improvements are mostly to weed out generic suggestions,
    ensure that code snippets are provided, and try to avoid making
    suggestions that involve changing things like libc or the linux
    kernel.

--- perfcopilot/commands/stacktrace.py
+++ perfcopilot/commands/stacktrace.py
@@ -16,20 +16,45 @@ def add_to_command_parser(subparsers):
 
 prompt = """You are assisting me with understanding a stack trace from a software profiler, such
 as pprof or perf record. My goal is to improve the software so that it runs faster and is more
-efficient. I will provide you with the stack trace that consumes the most CPU. A stack trace
-is a call stack, showing a list of functions that call each other. The stack trace is in reverse
-order.
+efficient. A stack trace is a call stack, showing a list of functions that call each other. The
+stack trace is in reverse order.
 
-First, summarise what the software is doing based on the stacktrace. Provide a technical
-description of the functions that are most likely to help a user understand what the
-stack trace is doing. In most cases this will mean explaining at least the details of the
-first three functions in the stack trace.
+I will provide you with a stack trace which is reported by the profiler as consuming a
+significant amount of CPU.
 
-Then outline include an explanation of any bottlenecks or performance issues that are present.
-Then, suggest actions I may take to fix bottlenecks or performance issues that are
-shown in the stack trace. For each action give a short and concise summary, then a more
-detailed explanation that includes examples to clarify what you mean. If there are multiple
-alternative actions then present them as a list.
+Assuming this stack trace is the most common stack trace encountered by a profiler,
+suggest ways to optimize or improve the system to make it more efficient. Types of
+improvements that would be useful to me are improvements that result in:
+
+- Higher performance so that the system runs faster or uses less CPU
+- Better memory efficient so that the system uses less RAM
+- Better storage efficient so that the system stores less data on disk.
+- Better network I/O efficiency so that less data is sent over the network
+- Better disk I/O efficiency so that less data is read and written from disk
+
+Make a maximum of five suggestions. Favour providing a small number of accurate, concise,
+concrete, technically correct and actionable suggestions, over a larger number of ones that do
+not have these properties.
+
+Your suggestions must meet all of the following criteria:
+1. Your suggestions should detailed, accurate, technical and include concrete examples.
+2. Your suggestions should be specific to the provided stack trace. Only make suggestions which
+are directly inspired by the content of the stack trace. Do not make generic suggestions.
+3. If you suggest replacing the function or library with a more efficient replacement you must
+suggest at least one concrete replacement.
+4. If you suggest making code changes, then show a code snippet as an example of what you mean,
+and explain why it is helpful.
+5. Do not suggest making changes to functions that are in large public libraries, like libc, or
+the Linux kernel.
+
+If you know of fewer than five ways to improve the performance the system, then provide fewer
+than five suggestions. If you do not know of any way in which to improve the performance then
+say "I do not know how to improve the performance of systems where this stack trace is consuming
+a significant amount of CPU".
+
+Do not suggest to use a CPU profiler or to profile the code. I have already profiled the code
+using a CPU profiler. You should favour stopping making suggestions over suggesting profiling
+the code with a CPU profiler.
 
 This is the stack trace:
 
